86ebd5e2f37b5c27227ea4a0e8e8b9ce8599a793
Koen Zweerts <zwk1ein@bosch.com>
2021-10-28 09:23:52 +0200

    added chained backend for primary cache and fetch fallback

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,7 @@
 import i18next from 'i18next';
-import Backend from 'i18next-http-backend';
+import Backend from 'i18next-chained-backend';
+import LocalStorageBackend from 'i18next-localstorage-backend';
+import HttpApi from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
@@ -16,6 +18,30 @@ i18next
 		},
 		interpolation: {
 			escapeValue: false, // not needed for react as it escapes by default
+		},
+		backend: {
+			backends: [LocalStorageBackend, HttpApi],
+			backendOptions: [
+				{
+					// prefix for stored languages
+					prefix: 'i18next_res_',
+
+					// expiration
+					expirationTime: 7 * 24 * 60 * 60 * 1000,
+
+					// Version applied to all languages, can be overriden using the option `versions`
+					defaultVersion: '',
+
+					// language versions
+					versions: {},
+
+					// can be either window.localStorage or window.sessionStorage. Default: window.localStorage
+					store: typeof window !== 'undefined' ? window.localStorage : null,
+				},
+				{
+					loadPath: '/locales/{{lng}}/{{ns}}.json',
+				},
+			],
 		},
 	});
 
